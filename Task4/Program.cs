9c8d6616f7d3c9ae06f206fc716071f9eb38d105
{{pijamaza}} <{{shockinglad@gmail.com}}>
2022-09-25 22:24:34 +0300

    task4 compliet

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Name = "Vadim";
+            user.surname = "Koval";
+            user.age = 23;
+            user.login = "kovalVD";
+            user.password = "973137";
+            user.registrationDate = DateTime.Now;
+            
+            user.ShowUserInfo();
+        }
+    }
+}
---
+++ Task4/User.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task4
+{
+    internal class User
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public string login;
+        public string password;
+        public DateTime registrationDate;
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+
+            get
+            {
+                return name;
+            }
+        }
+        public string Surname
+        {
+            set
+            {
+                surname = value;
+            }
+            get
+            {
+                return surname;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                age = value;
+            }
+
+            get
+            {
+                return age;
+            }
+        }
+
+        public string Login
+        {
+            set
+            {
+                login = value;
+
+            }
+            get
+            {
+                return login;
+            }
+        }
+
+        public string Password
+        {
+            set
+            {
+                password = value;
+            }
+            get
+            {
+                return password;
+            }
+        }
+
+        public DateTime RegistrationDate
+        {
+            set
+            {
+                registrationDate = value;
+            }
+            get
+            {
+                return registrationDate;
+            }
+        }
+        public void ShowUserInfo()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("User's name: {0} {1}", Name, Surname);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Login: {0}", Login);
+            Console.WriteLine("Registration date: {0}", RegistrationDate);
+        }
+    }
+}
